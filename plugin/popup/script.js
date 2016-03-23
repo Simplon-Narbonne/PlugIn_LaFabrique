6e26c7d1dc85c458cfcbab811d6f9846de427007
simplon-gregoryg <grexor@hotmail.com>
2016-03-23 10:15:19 +0100

    ajout premiere version du back-end

--- plugin/popup/script.js
+++ plugin/popup/script.js
@@ -1,6 +1,6 @@
 //Ajoute un evenement sur le clic du lien (redirection vers onglet deja ouvert)
 
-var element = document.querySelector('#scriptOnglet');
+/*var element = document.querySelector('#scriptOnglet');
 
 element.addEventListener('click', function() {
 
@@ -20,3 +20,4 @@ element.addEventListener('click', function() {
     }
   });
 }, false);
+*/
