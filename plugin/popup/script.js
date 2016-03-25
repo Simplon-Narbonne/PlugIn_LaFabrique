acf70505d72ab326227952a45ef2a3b6bda46abf
Bullich benjamin <bullich.benjamin@gmail.com>
2016-03-25 09:08:27 +0100

    css benj

--- plugin/popup/script.js
+++ plugin/popup/script.js
@@ -1,6 +1,6 @@
 //Ajoute un evenement sur le clic du lien (redirection vers onglet deja ouvert)
 
-var element = document.querySelector('#scriptOnglet');
+/*var element = document.querySelector('#scriptOnglet');
 
 element.addEventListener('click', function() {
 
@@ -19,4 +19,26 @@ element.addEventListener('click', function() {
       });
     }
   });
-}, false);
+}, false);*/
+
+var text = 'Variable globale !';
+
+var body = document.querySelector('body');
+
+var div = document.createElement('div');
+
+var p = document.createElement('p');
+
+var texte = document.createTextNode('on testetrynhrnhr');
+
+p.appenChild(texte);
+
+div.appenChild(p);
+
+body.appenChild(div);
+
+
+var btnVersSite = document.querySelector('#scripOnglet');
+btnVersSite.addEventListener('click', function() {
+        alert("oui oui ca marche !");
+    });
