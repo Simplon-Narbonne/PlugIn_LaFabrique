764da176141f497baa8dbc5a0cd3ed0883e047ea
simplon-gregoryg <grexor@hotmail.com>
2016-04-17 14:23:10 +0200

    ajout : hide on click sur chaque article dans le popup. Bug : ca n'ouvre plus les pages lors du clic

--- plugin/popup/plugfabrique.js
+++ plugin/popup/plugfabrique.js
@@ -3,18 +3,20 @@ function parseXML()
     var json_tabArticles = JSON.parse(localStorage.getItem("articles"));
     for(var i = 0; i < json_tabArticles.length; i++)
     {
+
+        getExistDiv = document.querySelector("#Contenu");
+        var dynDiv = "div" + (i+1);
+        createDivAuto = document.createElement(dynDiv);
+        createDivArticle = document.createElement('div');
+        createDivArticle.setAttribute("class", "article");
+
         if(json_tabArticles[i].show == false)
         {
+            getExistDiv.appendChild(createDivAuto);
             continue;
         }
         else
         {
-            getExistDiv = document.querySelector("#Contenu");
-            var dynDiv = "div" + (i+1);
-            createDivAuto = document.createElement(dynDiv);
-            createDivArticle = document.createElement('div');
-            createDivArticle.setAttribute("class", "article");
-
             var createLink = document.createElement("a");
             var dynLink = json_tabArticles[i].link;
             createLink.setAttribute("href", dynLink);
@@ -30,7 +32,7 @@ function parseXML()
             createTitle.appendChild(createTextNode);
             createDivArticle.appendChild(createTitle);
 
-            strDesc = r_DestroyTags(json_tabArticles[i].description);
+            strDesc = json_tabArticles[i].description;
             var createPara = document.createElement("p");
             createPara.setAttribute("class", "paragraphe");
             var setAtt_id = "innerP"+(i+1);
@@ -44,6 +46,36 @@ function parseXML()
 }
 
 parseXML();
+
+var hideDiv1 = document.querySelector("div1");
+var hideDiv2 = document.querySelector("div2");
+var hideDiv3 = document.querySelector("div3");
+
+hideDiv1.addEventListener("click", function(e){
+    var json_tabArticles = JSON.parse(localStorage.getItem("articles"));
+    json_tabArticles[0].show = false;
+    json_tabArticles = JSON.stringify(json_tabArticles);
+    localStorage.setItem("articles", json_tabArticles);
+    window.location.reload();
+}, false);
+
+hideDiv2.addEventListener("click", function(e){
+    var json_tabArticles = JSON.parse(localStorage.getItem("articles"));
+    json_tabArticles[1].show = false;
+    json_tabArticles = JSON.stringify(json_tabArticles);
+    localStorage.setItem("articles", json_tabArticles);
+    window.location.reload();
+}, false);
+
+hideDiv3.addEventListener("click", function(e){
+    var json_tabArticles = JSON.parse(localStorage.getItem("articles"));
+    json_tabArticles[2].show = false;
+    json_tabArticles = JSON.stringify(json_tabArticles);
+    localStorage.setItem("articles", json_tabArticles);
+    window.location.reload();
+}, false);
+
+
 /*if(localStorage.getItem("pf_originXML")) //pf = plugin fabrique
 {
     browser.runtime.getBackgroundPage(
